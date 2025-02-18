a95dbadaaa87e807bdc2c51ebd9d95f7c0962f94
Joao Monteiro <joaomonteiro@cge.pi.gov.br>
2025-02-18 13:31:28 -0300

    Iniciando navegação entre páginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import styles from "./page.module.css";
 import {CardPost} from "@/components/CardPost";
 import {Post} from "@/modules/Post";
 import logger from "@/logger";
+import Link from "next/link";
 
-async function getPosts(page: number, perPage: number = 6) {
+interface HomeProps {
+	searchParams: Promise<{ [key: string]: string | undefined }>;
+}
+
+async function getPosts(page: string, perPage: string = '6') {
 	const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=${perPage}`);
 	if (!response.ok) {
 		logger.error("Failed to fetch posts", response);
@@ -13,15 +18,18 @@ async function getPosts(page: number, perPage: number = 6) {
 	return response.json();
 }
 
-export default async function Home() {
+export default async function Home({searchParams}: HomeProps) {
+	const page = (await searchParams).page || '1';
 
-	const {data: posts} = await getPosts(1);
+	const {data: posts, prev, next} = await getPosts(page);
 
 	return (
 		<main className={styles.container + " " + styles.grid}>
 			{posts.map((post: Post) => (
 				<CardPost post={post} key={post.id}/>
 			))}
+			{prev && <Link href={`/?page=${prev}`}>Anterior</Link>}
+			{next && <Link href={`/?page=${next}`}>Próximo</Link>}
 		</main>
 	);
 }
---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default function post() {
+	return (
+		<div>
+			<h1>Post</h1>
+		</div>
+	);
+}
