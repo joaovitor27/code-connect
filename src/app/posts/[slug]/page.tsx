446f8bb2afcb36040bf0f0f8feb34d7af5892af6
joao <joaovitor.monteiro.188@gmail.com>
2025-02-18 16:29:26 -0300

    Concluíndo o primeiro modulo e adicionando configurações iniciais para o segundo modulo

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,39 @@
-export default function post() {
-	return (
-		<div>
-			<h1>Post</h1>
-		</div>
-	);
+import logger from "@/logger";
+import {remark} from "remark";
+import html from 'remark-html';
+import {CardPost} from "@/components/CardPost";
+
+import styles from './page.module.css';
+
+async function getPost(slug: string) {
+  const res = await fetch(`http://localhost:3042/posts?slug=${slug}`);
+  if (!res.ok) {
+    logger.error(`Failed to fetch post with slug ${slug}`);
+    return {};
+  }
+  logger.info(`Fetched post with slug ${slug}`);
+  const data = await res.json();
+  if (!data) {
+    logger.error(`Failed to parse post with slug ${slug}`);
+    return {};
+  }
+  const post = data[0];
+
+  const processedContent = await remark().use(html).process(post.markdown);
+  post.markdown = processedContent.toString();
+
+  return post;
+}
+
+export default async function PagePost({params,}: { params: Promise<{ slug: string }> }) {
+  const slug = (await params).slug
+
+  const post = await getPost(slug);
+    return (<div>
+        <CardPost post={post} highlight />
+        <h3 className={styles.subtitle}>Código:</h3>
+        <div className={styles.code}>
+            <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
+        </div>
+    </div>)
 }
--- src/components/CardPost/index.tsx
+++ src/components/CardPost/index.tsx
@@ -3,23 +3,26 @@ import {Post} from "@/modules/Post";
 import Image from "next/image";
 
 import styles from "./cardpost.module.css";
+import Link from "next/link";
 
 
-export function CardPost({post}: Readonly<{ post: Post }>) {
-	return (
-		<article className={styles.card}>
-			<header className={styles.header}>
-				<figure>
-					<Image src={post.cover} alt={post.title} width={438} height={133}/>
-				</figure>
-			</header>
-			<section className={styles.body}>
-				<h2>{post.title}</h2>
-				<p>{post.body}</p>
-			</section>
-			<footer className={styles.footer}>
-				<Avatar src={post.author.avatar} alt={post.author.username} name={post.author.username}/>
-			</footer>
-		</article>
-	);
+export function CardPost({post, highlight }: Readonly<{ post: Post, highlight?: boolean }>) {
+  return (
+    <Link href={`/posts/${post.slug}`} className={styles.link}>
+      <article className={styles.card}  style={{ width: highlight ? 993 : 486}}>
+        <header className={styles.header}>
+          <figure style={{ height: highlight ? 300 : 133}}>
+            <Image src={post.cover} alt={post.title} fill/>
+          </figure>
+        </header>
+        <section className={styles.body}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </section>
+        <footer className={styles.footer}>
+          <Avatar src={post.author.avatar} alt={post.author.username} name={post.author.username}/>
+        </footer>
+      </article>
+    </Link>
+  );
 }
